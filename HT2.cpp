2eb220929d7aa35c64037c4a32cb252c05d60722
Jackeline Benitez <jackelinebenitez112@gmail.com>
2021-02-25 00:47:12 -0600

    hoja finalizada

--- HT2.cpp
+++ HT2.cpp
@@ -4,8 +4,7 @@
 FILE *file = NULL;
 void CrearArchivo()
 {
-    file = fopen("HT2.dk", "r"); //abrimos el archivo para leer con r
-
+    file = fopen("HT2.dk", "r");
     if (file == NULL) //Verificamos si es nulo
     {
         file = fopen("HT2.dk", "w"); //si si hora si abrimos para escribir con w
@@ -44,6 +43,7 @@ int main()
         cin >> menu;
         switch (menu)
         {
+        /*-------------------------------------------------------------------------*/
         case 1:
             cout << "Ingrese el nombre ";
             cin >> name;
@@ -54,11 +54,11 @@ int main()
             {
                 posCuadrante = 0; //La posicion del cuadrante
             }
-            else if (age <= 50 && age > 25)
+            else if (age <= 50 )
             {
-                posCuadrante = sizeof(personaTemporal) * 25 * 1; //Aqui se multiplico por 29 que es el size of de mi struct, luego por 25 que es el tamaño del primer cuadrante
+                posCuadrante = sizeof(persona) * 25 * 1; //Aqui se multiplico por 29 que es el size of de mi struct, luego por 25 que es el tamaño del primer cuadrante
             }
-            else if (age <= 75 && age > 51)
+            else if (age <= 75 )
             {
                 posCuadrante = sizeof(persona) * 25 * 2; //Lo mismo aca, pero es por 2 por ser el cuadrante 3
             }
@@ -72,7 +72,6 @@ int main()
 
             if (file != NULL) // SI el archivo no esta vacio
             {
-
                 fread(&personaCuadrante, sizeof(personaCuadrante), 1, file); //Primero leemos nuestro objeto cuadrnte y no tiene nada
                 for (int i = 0; i < 25; i++)                                 //recorremos el cuadrante
                 {
@@ -84,37 +83,161 @@ int main()
                         break;                                                      //y salimos
                     }
                 }
-
                 fclose(file); //cerramos el archivo
-                //file = NULL;
                 printf("Persona guardada correctamente \n");
             }
             else
             {
                 printf("Error!\n No se puede acceder al archivo, no creado\n");
             }
-
             break;
+             /*-------------------------------------------------------------------------*/
+        case 2:
+        file = fopen("HT2.dk", "rb+");
+            cout << "Ingrese la edad a modificar ";
+            cin >> age;
+            cout << "Ingrese el nuevo nombre";
+            cin >> nombreM;
+            cout<<"INgrese la nueva edad";
+            cin>>edadM;
+            
+            
+            cout<<age;
+            fseek(file, 0, SEEK_SET); //Aqui nos posiciones en el cuadrante correspondiente a la edad
+            personaTemporal.edad = edadM;          //Y en nuestra variable temporal guardamos la edad y el nombre recien ingresado
+            strcpy(personaTemporal.nombre, nombreM);
+
+            if (file != NULL) // SI el archivo no esta vacio
+            {
+                fread(&personaT, sizeof(personaT), 1, file); //Primero leemos nuestro objeto cuadrnte y no tiene nada
+                for (int i = 0; i <100; i++)                                 //recorremos el cuadrante
+                {
+                    if (personaT[i].edad == age) //y encontramos el primer espacio vacio
+                    {
+                        cout<<"encontramos la edad perra";
+                        cout << "\n edad en esa posicion";
+                        cout<< personaT[i].edad ;
+                        cout << "\n ";
+                        posPersona = sizeof(persona) * i;                   //la posicion donde se va ubicar, es el tamaño de mi struct por la posicion de mi cuadrante
+                        fseek(file,  posPersona, SEEK_SET);      
+                        cout<<personaTemporal.nombre;  
+                        cout << "\n";
+                         cout<<personaTemporal.edad;     
+                         cout << "\n pos ";
+                         cout<<posPersona;
+                         cout << "\n";           //y ahora nuestro puntero debe estar en el inicio de ese cuadrante mas los espacios donde se debe ubicar en ese cuadrante
+                        fwrite(&personaTemporal, sizeof(personaTemporal), 1, file); // escribimos lo que tenemos en el struct
+                        break;                                                      //y salimos
+                    }
+                }
+                fclose(file); //cerramos el archivo
+                printf("Persona modificADA correctamente \n");
+            }
+            else
+            {
+                printf("Error!\n No se puede acceder al archivo, no creado\n");
+            }
+            break;
+        /*-------------------------------------------------------------------------*/
+        /*-------------------------------------------------------------------------*/
         case 3:
-            cout << "Ingrese el nombre de la persona que quiere eliminar";
-            cin >> nombre;
-           
+        file = fopen("HT2.dk", "rb+");
+        cout << "Ingrese la edad a eliminar ";
+            cin >> age;
+            edadE=-1;
+            strcpy(nombreE,"");
 
+            if (age <= 25) // verificaciones para ver en que cuadrante se ubica
+            {
+                posCuadrante = 0; //La posicion del cuadrante
+            }
+            else if (age <= 50 )
+            {
+                posCuadrante = sizeof(persona) * 25 * 1; //Aqui se multiplico por 29 que es el size of de mi struct, luego por 25 que es el tamaño del primer cuadrante
+            }
+            else if (age <= 75 )
+            {
+                posCuadrante = sizeof(persona) * 25 * 2; //Lo mismo aca, pero es por 2 por ser el cuadrante 3
+            }
+            else
+            {
+                posCuadrante = sizeof(persona) * 25 * 3;
+            }
+            fseek(file, posCuadrante, SEEK_SET); //Aqui nos posiciones en el cuadrante correspondiente a la edad
+            personaTemporal.edad = edadE;          //Y en nuestra variable temporal guardamos la edad y el nombre recien ingresado
+            strcpy(personaTemporal.nombre, nombreE);
+
+            if (file != NULL) // SI el archivo no esta vacio
+            {
+                fread(&personaCuadrante, sizeof(personaCuadrante), 1, file); //Primero leemos nuestro objeto cuadrnte y no tiene nada
+                for (int i = 0; i < 25; i++)                                 //recorremos el cuadrante
+                {
+                    if (personaCuadrante[i].edad == age) //y encontramos el primer espacio vacio
+                    {
+                        cout<<"si entre";
+                        posPersona = sizeof(personaTemporal) * i;                   //la posicion donde se va ubicar, es el tamaño de mi struct por la posicion de mi cuadrante
+                        fseek(file, posCuadrante + posPersona, SEEK_SET);           //y ahora nuestro puntero debe estar en el inicio de ese cuadrante mas los espacios donde se debe ubicar en ese cuadrante
+                        fwrite(&personaTemporal, sizeof(personaTemporal), 1, file); // escribimos lo que tenemos en el struct
+                        break;                                                      //y salimos
+                    }
+                }
+                fclose(file); //cerramos el archivo
+                printf("Persona eliminada correctamente \n");
+            }
+            else
+            {
+                printf("Error!\n No se puede acceder al archivo, no creado\n");
+            }
             break;
+
+        /*-------------------------------------------------------------------------*/
         case 4:
-            file = fopen("HT2.dk", "r"); //para verlo debemos volverlo a abrir
+            cout << "\n\n\n";
+            file = fopen("HT2.dk", "rb+"); //para verlo debemos volverlo a abrir
             fseek(file, 0, SEEK_SET);    //posicionarnos en cero
 
             //leemos el archivo binario y lo pasamos al arreglo temporal
             fread(&personaT, sizeof(personaT), 1, file); //leemos nuestro arreglo de 100
+            cout << "Edades de 0-25\n";
             for (int i = 0; i < 25; i++)
             {
-                std::cout << "nombre: " << personaT[i].nombre << " edad: " << personaT[i].edad << " \n";
+                if (personaT[i].nombre != "" && personaT[i].edad != -1)
+                {
+                    std::cout << "nombre: " << personaT[i].nombre << " edad: " << personaT[i].edad << " \n";
+                }
             }
-
-            //cerramos el archivo y liberamos el puntero
+            cout << "\n\n\n";
+            
+            cout << "Edades de 26-50\n";
+            for (int j = 25; j < 50; j++)
+            {
+                if (personaT[j].nombre != "" && personaT[j].edad != -1)
+                {
+                    std::cout << "nombre: " << personaT[j].nombre << " edad: " << personaT[j].edad << " \n";
+                }
+            }
+            cout << "\n\n\n";
+            
+            cout << "Edades de 51-75\n";
+            for (int k = 50; k <75; k++)
+            {
+                if (personaT[k].nombre != "" && personaT[k].edad != -1)
+                {
+                    std::cout << "nombre: " << personaT[k].nombre << " edad: " << personaT[k].edad << " \n";
+                }
+            }
+            cout << "\n\n\n";
+           
+            cout << "Edades de 76-100\n";
+            for (int l = 75; l <100; l++)
+            {
+                if (personaT[l].nombre != "" && personaT[l].edad != -1)
+                {
+                    std::cout << "nombre: " << personaT[l].nombre << " edad: " << personaT[l].edad << " \n";
+                }
+            }
+            cout << "\n\n\n";
             fclose(file);
-            // file = NULL;
 
             break;
         default:
@@ -124,83 +247,3 @@ int main()
 
     return 0;
 }
-//JAsystem("clear");
-
-/*    
-        cin >> opcion;
-
-        if(opcion==1)
-        {
-        
-
-            cout << "Ingresar el numero de personas que se va a ingresar \n";
-            cin >> cantpersonas;
-            persona personas[cantpersonas];
-
-            for (int i = 0; i < cantpersonas; i++)
-            {
-                cout << "Ingrese el nombre ";
-                cin >> name;
-                cout << "Ingrese la edad ";
-                cin >> age;
-
-                personas[i].edad = age;
-
-                //sprintf(name, "Persona-%d", i + 1);
-                strcpy(personas[i].nombre, name);
-            }
-            for (int i = 0; i < cantpersonas; i++)
-            {
-
-                cout << (i + 1) << ") nombre: " << personas[i].nombre << " edad: " << personas[i].edad << " \n";
-            }
-            fwrite(&personas, sizeof(personas), 1, file);
-
-            //cerramos el archivo y liberamos el puntero
-            fclose(file);
-            file = NULL;
-            main();
-
-
-        }else if(opcion==4){
-
-
-            // MOdificar
-
-            
-
-            char entrada[10];
-            while (true)
-            {
-                std::cout << "\nObtener persona (del 1 al " << cantpersonas << ")\n";
-                //guardamos la entrada de la consola en el arreglo
-                scanf(" %[^\n]", entrada);
-                //convertimos el arrglo a un entero
-                int posicion = atoi(&entrada[0]) - 1;
-
-                //abre el archivo binario(b) para lectura
-                file = fopen("HT2.dk", "rb+");
-
-                if (file == NULL)
-                {
-                    std::cout << "Ocurrio un error al acceder al archivo.\n";
-                    return 0;
-                }
-
-                //arreglo temporal donde vamos a guardar el contenido del archivo bianrio
-                persona tmp[cantpersonas];
-                //posicionamos el puntero, (puntero al archivo, posicion del puntero del archivo, operacion)
-                fseek(file, 0, SEEK_SET);
-                //leemos el archivo binario y lo pasamos al arreglo temporal
-                fread(&tmp, sizeof(tmp), 1, file);
-
-                //cerramos el archivo y liberamos el puntero
-                fclose(file);
-                file = NULL;
-
-                //obtenemos a la persona deseada
-                cout << "nombre: " << tmp[posicion].nombre << " edad: " << tmp[posicion].edad << " \n";
-            }
-
-        }
-    }*/
--- HT2.h
+++ HT2.h
@@ -6,10 +6,7 @@
 #include <cstdlib>
 
 using namespace std;
-int tam;
 char name[25];
-int cantpersonas = 1;
-
 int age;
 
 typedef struct
@@ -19,8 +16,15 @@ typedef struct
 } persona;
 
 persona personaT[100];
+persona personaE[100];
 persona personaCuadrante[25];
 int posCuadrante;
 int posPersona;
+int posPersona1;
 
 char nombre[25];
+int edadE;
+char nombreE[25];
+
+int edadM;
+char nombreM[25];
